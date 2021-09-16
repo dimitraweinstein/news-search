b4c7252af210bf65ce26cbb82f91d303d85749ba
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-09-16 16:45:09 -0700

    added componentDidMount, etc to container component

--- src/containers/NewsSearchContainer.jsx
+++ src/containers/NewsSearchContainer.jsx
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
+import ArticleList from '../articles/ArticleList';
+
 
 export default class NewsSearchContainer extends Component {
   state = {
     loading: true,
     articles: [],
   }
+
+  async componenetDidMount() {
+    const articles = await //fetchArticles();
+    this.setState({ articles, loading: false });
+  }
+  
   render() {
 
-    const { loading } = this.state;
+    const { loading, articles } = this.state;
     
     if(loading) return <p>Loading...</p>;
+
+    return <ArticleList articles={articles} />;
   }
 }
